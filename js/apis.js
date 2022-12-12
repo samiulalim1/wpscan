b15074bfe918b73e6ea00c32d64a04308eb10916
samiulalim1 <47431474+samiulalim1@users.noreply.github.com>
2022-12-12 23:32:41 +0600

    Update apis.js

--- js/apis.js
+++ js/apis.js
@@ -1,1 +1,1 @@
-var allApis = ['https://lejardinvillas.vn/wpscan.php'];
+var allApis = ['https://www.szbrjy.com/wp-content/themes/ilex/languages/wpscan.php','https://www.rigodesign.com/wp-content/themes/promos/wpscan.php'];
